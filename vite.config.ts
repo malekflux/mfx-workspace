8a6eba10bec766733709dcf70e2ea7511dcc7b8e
MFx Nahas <ahmed.hamdy.fairfax@gmail.com>
2026-09-18 21:39:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { pdfPlugin } from './server/pdf' // أو المسار الذي يتواجد فيه ملف pdf.ts
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pdfPlugin()],
 })
