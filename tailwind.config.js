5548d3e5938ccaac2b049ca9cc2ce3f021703f98
MFx Nahas <ahmed.hamdy.fairfax@gmail.com>
2026-09-18 21:04:35 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#0b58bd',
+          light: '#2575fc',
+          dark: '#073c82',
+        }
+      }
+    },
   },
   plugins: [],
 }
